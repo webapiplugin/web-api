cebcdec3daf6747aacd0d6480349f754fbd08a96
Laptop <fabian.rudolf.i19c@nordakademie.org>
2023-05-19 00:24:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# ChatGPT Plugin Bibliography Generator
+
+#### Simplifying Web Interaction with ChatGPT
+Make any web request via OpenAI models.
+
+# Embedding Bibliography Plugin into ChatGPT
+
+## Prerequisites
+- Access to the ChatGPT web interface where plugins can be added.
+
+## Steps
+
+1. **Access the Plugin Management Section.** Navigate to the section of the ChatGPT web interface where you can manage plugins.
+
+2. **Add a New Plugin.** Click on the 'Add Plugin' button.
+
+3. **Enter the Plugin URL.** In the field provided, enter the URL of the bibliography plugin: `https://webapiplugin-1-d3545675.deta.app/`
+
+4. **Confirm Addition of the Plugin.** Click the 'Confirm' button to finalize the addition of the new plugin.
+
+5. **Test the Plugin.** Once the plugin is added, it should be available for use with ChatGPT. Test it by initiating a conversation related to bibliography or citation search.
+
+Now, you can ask ChatGPT to find publications for a topic, suggest DOIs or save publications to your Zotero project.
+
+You can even try the Bibliography Generator Plugin API without access to ChatGPT here:
+```
+https://webapiplugin-1-d3545675.deta.app/freetext_to_bibtex?search_term={<enter_your_search_query>}
+```
+
+The OpenAPI manifest YAML can be accessed here:
+
+[https://webapiplugin-1-d3545675.deta.app/openapi.yaml](https://webapiplugin-1-d3545675.deta.app/openapi.yaml)
+
+[Legal notes](https://webapiplugin-1-d3545675.deta.app/static/legal.html)
+[Privacy notes](https://webapiplugin-1-d3545675.deta.app/static/privacy.html)
+
+For further information, contact [bibliography_generator@proton.me](mailto:bibliography_generator@proton.me)
---
+++ main.py
@@ -0,0 +1,136 @@
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.openapi.utils import get_openapi
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse, Response, JSONResponse, RedirectResponse
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
+from pydantic import HttpUrl, ValidationError
+from html_sanitizer import Sanitizer
+import json
+import httpx
+import requests
+from requests.exceptions import RequestException
+from typing import List, Dict
+import asyncio
+import uvicorn
+import os
+from urllib.parse import quote
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(debug=False)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["localhost", "0.0.0.0", "chat.openai.com", "https://webapiplugin-1-d3545675.deta.app/"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Content-Disposition"]
+)
+
+@app.middleware("http")
+async def add_cors_header(request: Request, call_next):
+    response = await call_next(request)
+    allowed_origin = request.headers.get("Origin")
+    response.headers["Access-Control-Allow-Origin"] = allowed_origin or "*"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    return response
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="https://github.com/webapiplugin/web-api")
+
+@app.route("/.well-known/ai-plugin.json", methods=["GET", "OPTIONS"])
+async def options_handler(request: Request):
+    if request.method == "GET":
+        try:
+            return FileResponse("./.well-known/ai-plugin.json")
+        except FileNotFoundError:
+            response = JSONResponse(content={"error": "File not found"}, status_code=404)
+            response.headers["Access-Control-Allow-Origin"] = request.headers["Host"]
+            return response
+    elif request.method == "OPTIONS":
+        try:
+            with open("./.well-known/ai-plugin.json") as f:
+                text = f.read()
+                response = JSONResponse(content=text, media_type="text/json")
+                response.headers["Access-Control-Allow-Origin"] = request.headers["Host"]
+                return response
+        except FileNotFoundError:
+            return JSONResponse(content={"error": "File not found"}, status_code=404)
+
+def sanitize(data):
+    if isinstance(data, str):
+        return data.encode('charmap', 'ignore').decode('charmap')
+    elif isinstance(data, dict):
+        return {key: sanitize(value) for key, value in data.items()}
+    elif isinstance(data, list):
+        return [sanitize(element) for element in data]
+    else:
+        return data
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Bibliography Generator",
+        version="1.0",
+        description="Democratizing access to scientific research with natural language using Crossref and Zotero",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = openapi_spec
+
+sanitizer = Sanitizer()
+
+@app.on_event("startup")
+async def startup():
+    # Configure the rate limiter to allow 10 requests per minute.
+    await FastAPILimiter.init("10/minute")
+
+async def proxy_request(url: str, method: str, data: str, headers: str, request: Request):
+    try:
+        if method.lower() == 'get':
+            response = requests.get(url, headers=headers)
+        elif method.lower() == 'post':
+            response = requests.post(url, data=data, headers=headers)
+        elif method.lower() == 'put':
+            response = requests.put(url, data=data, headers=headers)
+        elif method.lower() == 'delete':
+            response = requests.delete(url, headers=headers)
+        else:
+            raise HTTPException(status_code=400, detail="Invalid method")
+    except RequestException:
+        raise HTTPException(status_code=500, detail="Error occurred during request")
+
+    # Sanitize the response body to prevent potential XSS attacks.
+    sanitized_body = sanitizer.sanitize(response.text)
+
+    return {"status_code": response.status_code, "response_body": sanitized_body}
+
+@app.post("/request")
+async def wrapper_request(url: str, method: str, data: str = None, headers: str = None, request: Request = None, dependencies=[Depends(RateLimiter(times=2, seconds=5))]):
+    # Sanitize inputs
+    url = sanitizer.sanitize(url)
+    method = sanitizer.sanitize(method)
+    data = sanitizer.sanitize(data) if data else None
+    headers = sanitizer.sanitize(headers) if headers else None
+    
+    # Validate url and method
+    if method.lower() not in ['get', 'post', 'put', 'delete']:
+        raise HTTPException(status_code=400, detail="Invalid method")
+    try:
+        HttpUrl(url=url)
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    return await proxy_request(url, method, data, headers, request)
+
+
+if __name__ == '__main__':
+    os.system("uvicorn main:app --host 0.0.0.0 --port 5003 --reload")
